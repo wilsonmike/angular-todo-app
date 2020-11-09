7c0177d3b5f1f048905e52c9c44ec5aa7874800a
mike <michaelwilsonsj@hotmail.com>
2020-11-09 17:10:12 -0500

    added add delete and filter functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodoComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TodoComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Todo } from '../interface/todo';
 
 @Component({
@@ -25,7 +26,53 @@ export class TodoComponent implements OnInit {
       completed: false,
     },
   ];
+  todoSearchTerm: string = '';
   constructor() {}
 
   ngOnInit(): void {}
+  addTodos = (form: NgForm): void => {
+    let newTask: Todo = {
+      task: form.value.addTodo,
+      completed: false,
+    };
+    this.todos.push(newTask);
+  };
+  // adoptedAnimal = (type: string, index: number): void => {
+  //   type === 'cat' ? this.cats.splice(index, 1) : this.dogs.splice(index, 1);
+  removeTask = (index: number): void => {
+    this.todos.splice(index, 1);
+  };
+  completeTask = (index: number): void => {
+    this.todos[index].completed = true;
+  };
+  filterTodos = (): Todo[] => {
+    if (!this.todoSearchTerm) {
+      return this.todos;
+    } else {
+      return this.todos.filter((todo) => {
+        let currentTodoName = todo.task.toLowerCase().trim();
+        return currentTodoName.includes(
+          this.todoSearchTerm.toLowerCase().trim()
+        );
+      });
+    }
+  };
+
+  setTodoSearchTerm = (form: NgForm): void => {
+    this.todoSearchTerm = form.value.searchTerm;
+  };
+  // filterCats = (): Animal[] => {
+  //   if (!this.catSearchTerm) {
+  //     return this.cats;
+  //   } else {
+  //     return this.cats.filter((cat) => {
+  //       let currentCatName = cat.name.toLowerCase().trim();
+  //       return currentCatName.includes(this.catSearchTerm.toLowerCase().trim());
+  //     });
+  //   }
+  // };
+
+  // setCatSearchTerm = (form: NgForm): void => {
+  //   this.catSearchTerm = form.value.searchTerm;
+  // };
 }
